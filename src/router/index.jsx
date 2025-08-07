bb14f6bbece867de5c3f4a07842564b8be2e7ba1
María Guadalupe <coralucy12@gmail.com>
2025-08-07 15:22:58 -0600

    Update styles and add PanelPrincipal.jsx

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Inicio from '../pages/Inicio';
+import PanelPrincipal from '../pages/PanelPrincipal';
+
 
 /**
  * File: index.jsx
  * Created by: María Guadalupe Martínez Jiménez (mmartinezj004@uaemex.mx)
  * Created on: 2025-07-04
- * Last modified: 2025-07-17
+ * Last modified: 2025-08-05
  * Description: Main router component for the application, handling all route definitions.
  */
 
@@ -14,6 +16,8 @@ function AppRouter() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Inicio />} />
+        <Route path="/panel-principal" element={<PanelPrincipal />} />
+        {/* Add more routes as needed */}
       </Routes>
     </BrowserRouter>
   );
